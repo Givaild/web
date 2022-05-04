c623e0529cae84b0b9e6e94302dc7d2bac3bd819
Givaildo de Andrade <givaildo.gliveira@synvia.com>
2022-05-04 10:25:09 -0300

    fix/responsive/Form/Button

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,8 +1,8 @@
 export function WidgetForm(){
     return(
-        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg">
+        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
 
-            Hello Word
+            Hello Word !
         </div>
     )
 }
